f35734cfce88b230742f93d89f0cd3f9a54263ae
JaimeLHF <102550891+JaimeLHF@users.noreply.github.com>
2023-02-17 16:38:45 -0300

    Update input.js

--- JS/input.js
+++ JS/input.js
@@ -339,16 +339,4 @@ window.addEventListener('load', function () {
     loading.style.display = 'none';
 });
 
-const beforeUnloadListener = (event) => {
-    event.preventDefault();
-    return event.returnValue = "Are you sure you want to exit?";
-  };
-
-  input.addEventListener("input", (event) => {
-    if (event.target.value !== "") {
-      addEventListener("beforeunload", beforeUnloadListener, {capture: true});
-    } else if (event.target.value == "") {
-      removeEventListener("beforeunload", beforeUnloadListener, {capture: true});
-    }
-  });
 
